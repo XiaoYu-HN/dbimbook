9a5fd7db0549aa6e4c7ac6b9283e7a827dcdbc57
XiaoYu-HN <xiaojinghang@foxmail.com>
2021-03-03 02:44:48 +0000

    add query test in procedure

--- chap04/p_compratio.sql
+++ chap04/p_compratio.sql
@@ -2,7 +2,7 @@
 -- sqlplus sys@orclpdb1 as sysdba @p_compratio.sql
 
 drop table ssb.compresult;
-create table ssb.compresult(complevel varchar(32), poptime number, compratio number);
+create table ssb.compresult(complevel varchar(32), poptime number, compratio number, querytime number);
 
 grant select on v_$im_segments to ssb;
 grant execute on DBMS_INMEMORY_ADMIN to ssb;
@@ -12,22 +12,23 @@ create or replace PROCEDURE ssb.p_compratio (
     complevel  IN  VARCHAR2,
     num_run    IN  NUMBER
 ) AS
-    poptime    NUMBER;
-    dmlstr     VARCHAR2(100);
+    ptime    NUMBER;
+    qtime    NUMBER;
     popresult  NUMBER;
     compratio  NUMBER;
+	tmpsum	NUMBER;
 BEGIN
     FOR i IN 1..num_run LOOP
         EXECUTE IMMEDIATE 'ALTER TABLE lineorder NO INMEMORY';
         EXECUTE IMMEDIATE 'ALTER TABLE lineorder INMEMORY ' || complevel;
-        poptime := dbms_utility.get_time;
+        ptime := dbms_utility.get_time;
         SELECT
             dbms_inmemory_admin.populate_wait(priority => 'NONE')
         INTO popresult
         FROM
             dual;
 
-        poptime := ( dbms_utility.get_time - poptime ) / 100;
+        ptime := ( dbms_utility.get_time - ptime ) / 100;
         
         SELECT
             round(bytes / inmemory_size, 2)
@@ -35,15 +36,22 @@ BEGIN
         FROM
             v$im_segments;
 
+        qtime := dbms_utility.get_time;
+	select min(lo_quantity) + max(lo_ordtotalprice) + avg(lo_ordtotalprice) + avg(lo_quantity)  into tmpsum from lineorder;
+        qtime := ( dbms_utility.get_time - qtime );
+
         INSERT INTO compresult VALUES (
             complevel,
-            poptime,
-            compratio
+            ptime,
+            compratio,
+			qtime
         );
 
         COMMIT;
     END LOOP;
 END p_compratio;
 /
+
+show errors;
 
 exit
