14babe3e14f89872aa5594973ace046f7ad97014
XiaoYu-HN <xiaojinghang@foxmail.com>
2021-03-03 23:30:15 +0800

    adjust SGA to 3000M and IMCS to 2000M

--- chap04/dbim_enable.sh
+++ chap04/dbim_enable.sh
@@ -1,6 +1,9 @@
+# make sure your OS RAM >= 5GB
 sqlplus -S / as sysdba <<-EOF
 
-ALTER SYSTEM SET inmemory_size = 1G SCOPE = SPFILE;
+ALTER system SET sga_target = 3000M SCOPE = SPFILE ;
+ALTER system SET sga_max_size = 3000M SCOPE = SPFILE ;
+ALTER system SET inmemory_size = 2000m SCOPE = SPFILE ;
 
 SHUTDOWN IMMEDIATE
 
