84ccf0a8c723f2f1d92d5dad88f0ed4b7691cc1c
XiaoYu-HN <xiaojinghang@foxmail.com>
2021-06-06 13:07:14 +0800

    spatial script

---
+++ chap07/im_spatial.sql
@@ -0,0 +1,51 @@
+drop table t2;
+DELETE FROM USER_SDO_GEOM_METADATA;
+create table t2( id number, geometry SDO_GEOMETRY);
+
+begin
+for i in 1..1000
+loop
+	    INSERT INTO t2 VALUES (
+        i,
+        sdo_geometry(2001,                  
+         4326,                              
+         sdo_point_type(dbms_random.value(1,360) - 180, dbms_random.value(1,360) -90, null),
+                     NULL,               
+                     NULL               
+                     )
+    );
+
+    if mod(i, 5000) = 0 then
+        commit;
+    end if;
+end loop;
+commit;
+end;
+/
+
+    
+INSERT INTO USER_SDO_GEOM_METADATA VALUES (
+   'T2',
+   'GEOMETRY',
+   SDO_DIM_ARRAY(
+       SDO_DIM_ELEMENT(
+           'x', -180, 180, 0.05
+       ), SDO_DIM_ELEMENT(
+           'y', -90, 90, 0.05
+       )
+   ),
+   4326
+   );
+commit;
+
+CREATE INDEX t2_sidx ON
+    t2 (
+        geometry
+    )
+        INDEXTYPE IS mdsys.spatial_index PARAMETERS (' SDO_INDX_DIMS=2');
+
+select a.id, b.id from t2 a, t2 b
+where a.id != b.id and SDO_WITHIN_DISTANCE(
+        a.GEOMETRY, b.GEOMETRY, 'distance=50 unit=km'
+    ) = 'TRUE';
+    
--- showplan.sql
+++ showplan.sql
@@ -1,2 +1,3 @@
 set lines 200
+set pages 9999
 select * from table(dbms_xplan.display_cursor());
