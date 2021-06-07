71776d17f7fe206472883531008fff93a8ee5140
XiaoYu-HN <xiaojinghang@foxmail.com>
2021-06-07 09:16:36 +0800

    IM spatial test

--- chap07/im_spatial.sql
+++ chap07/im_spatial.sql
@@ -1,19 +1,16 @@
 drop table t2;
-DELETE FROM USER_SDO_GEOM_METADATA;
+delete from user_sdo_geom_metadata where table_name = 'T2';
 create table t2( id number, geometry SDO_GEOMETRY);
 
 begin
 for i in 1..1000000
 loop
-	    INSERT INTO t2 VALUES (
-        i,
-        sdo_geometry(2001,                  
-         4326,                              
-         sdo_point_type(dbms_random.value(1,360) - 180, dbms_random.value(1,360) -90, null),
-                     NULL,               
-                     NULL               
-                     )
-    );
+	INSERT INTO t2 VALUES (
+    i,
+    sdo_geometry(2001, 4326, sdo_point_type(dbms_random.value(1, 360) - 180, dbms_random.value(1, 360) - 90, NULL),
+                 NULL,
+                 NULL)
+	);
 
     if mod(i, 5000) = 0 then
         commit;
@@ -23,24 +20,35 @@ commit;
 end;
 /
 
-    
-INSERT INTO USER_SDO_GEOM_METADATA VALUES (
-   'T2',
-   'GEOMETRY',
-   SDO_DIM_ARRAY(
-       SDO_DIM_ELEMENT(
-           'x', -180, 180, 0.05
-       ), SDO_DIM_ELEMENT(
-           'y', -90, 90, 0.05
-       )
-   ),
-   4326
-   );
+INSERT INTO user_sdo_geom_metadata VALUES (
+    'T2',
+    'GEOMETRY',
+    sdo_dim_array(sdo_dim_element('x', - 180, 180, 0.05), sdo_dim_element('y', - 90, 90, 0.05)),
+    4326
+);    
 commit;
 
 CREATE INDEX t2_sidx ON
     t2 (
         geometry
     )
-        INDEXTYPE IS mdsys.spatial_index PARAMETERS (' SDO_INDX_DIMS=2');
+        INDEXTYPE IS mdsys.spatial_index;
 
+SELECT
+    id
+FROM
+    t2
+WHERE
+    sdo_filter(geometry, sdo_geometry(2001, 4326, sdo_point_type(- 72.432246, 156.378981, NULL), NULL, NULL)) = 'TRUE';
+
+/*
+SELECT
+    a.id,
+    b.id
+FROM
+    t3  a,
+    t3  b
+WHERE
+        a.id != b.id
+    AND sdo_within_distance(a.geometry, b.geometry, 'distance=50 unit=km') = 'TRUE';
+*/
