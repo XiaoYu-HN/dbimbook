3966ec5f527d5e095d4479e00aafe88633a095d7
XiaoYu-HN <xiaojinghang@foxmail.com>
2021-07-01 10:26:12 +0800

    change the sql order

--- chap08/racfail.sql
+++ chap08/racfail.sql
@@ -4,15 +4,13 @@ COL segment_name FOR a12
 
 SELECT
     inst_id,
-    imcu_addr,
-    num_rows,
-    to_char(timestamp, 'HH24:MI:SS') AS timestamp
+    segment_name,
+    inmemory_size / 1024 / 1024      im_mb,
+    bytes / 1024 / 1024              disk_mb,
+    bytes_not_populated,
+    populate_status              AS pop_status
 FROM
-    gv$im_header
-WHERE
-    is_head_piece = 1
-ORDER BY
-    initial_timestamp;
+    gv$im_segments;
 
 SELECT
     inst_id,
@@ -23,13 +21,15 @@ WHERE
     is_head_piece = 1
 GROUP BY
     inst_id;
-
+    
 SELECT
     inst_id,
-    segment_name,
-    inmemory_size / 1024 / 1024      im_mb,
-    bytes / 1024 / 1024              disk_mb,
-    bytes_not_populated,
-    populate_status              AS pop_status
+    imcu_addr,
+    num_rows,
+    to_char(timestamp, 'HH24:MI:SS') AS timestamp
 FROM
-    gv$im_segments;
+    gv$im_header
+WHERE
+    is_head_piece = 1
+ORDER BY
+    initial_timestamp;
