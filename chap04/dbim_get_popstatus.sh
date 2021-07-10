5547038c56f4ba33aa70114fcec76fbfa0d099c5
XiaoYu-HN <xiaojinghang@foxmail.com>
2021-07-10 09:01:16 +0800

    minor change

--- chap04/dbim_get_popstatus.sh
+++ chap04/dbim_get_popstatus.sh
@@ -3,7 +3,7 @@ sqlplus -S /nolog << 'EOF'
 @../userlogin.sql
 
 COL owner FORMAT A6
-COL segment_name heading 'SEGMENT|NAME' FORMAT A10
+COL segment_name heading 'SEGMENT|NAME' FORMAT A12
 COL populate_status heading 'POPULATED|STATUS'
 COL "IN_MEM_SIZE(KB)" heading 'IN MEM|SIZE(KB)' FORMAT 999,999,999
 COL "ON_DISK_SIZE(KB)" heading 'ON DISK|SIZE(KB)' FORMAT 999,999,999
