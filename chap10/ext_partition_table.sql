9f388c2fb88c3da1bcfce28f41686de7105a7f1f
XiaoYu-HN <xiaojinghang@foxmail.com>
2021-07-13 23:21:30 +0800

    add system view SQL

--- chap10/ext_partition_table.sql
+++ chap10/ext_partition_table.sql
@@ -15,23 +15,25 @@ DEFAULT DIRECTORY default_dir
 PARTITION BY RANGE (t) 
 (
 PARTITION p2020 VALUES LESS THAN (TO_DATE('01-Jan-2021','dd-MON-yyyy')) LOCATION ('2020.csv'),
-PARTITION p2021 VALUES LESS THAN (TO_DATE('01-Jan-2022','dd-MON-yyyy')) LOCATION ('2021.csv'),
-PARTITION pmax VALUES LESS THAN (MAXVALUE) 
+PARTITION p2021 VALUES LESS THAN (TO_DATE('01-Jan-2022','dd-MON-yyyy')) LOCATION ('2021.csv')
 );
 
 INSERT INTO test_ext_partition VALUES (
     10000,
-    TO_DATE('01-Jan-2028', 'dd-MON-yyyy')
+    TO_DATE('01-Feb-2021', 'dd-MON-yyyy')
 );
 
-UPDATE test_ext_partition
-SET
-    t = TO_DATE('01-Jan-2029', 'dd-MON-yyyy')
-WHERE
-    id = 10000;
-
 SET lines 200
+SET pages 9999
 COL partition_name FOR a14
-COL tablespace_name FOR a15
+COL high_value FOR a23
 COL table_name FOR a20
-SELECT table_name, partition_name, high_value, tablespace_name, read_only FROM user_tab_partitions;
+COL type_name FOR a20
+COL default_directory_name FOR a30
+
+SELECT table_name, partition_name, partition_position, high_value FROM user_tab_partitions;
+
+SELECT external, hybrid FROM user_tables WHERE table_name = 'TEST_EXT_PARTITION';
+
+SELECT table_name, type_name, default_directory_name FROM all_xternal_part_tables;
+
