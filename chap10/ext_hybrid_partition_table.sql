35a23f579bb197c0ef7706f4539602aed13c5a2e
XiaoYu-HN <xiaojinghang@foxmail.com>
2021-07-14 19:24:52 +0800

    make table name shorter

--- chap10/ext_hybrid_partition_table.sql
+++ chap10/ext_hybrid_partition_table.sql
@@ -2,8 +2,8 @@ DROP DIRECTORY default_dir;
 
 CREATE DIRECTORY default_dir AS '/tmp';
 
-DROP TABLE test_hypt_partition;
-CREATE TABLE test_hypt_partition (
+DROP TABLE test_hypt_part;
+CREATE TABLE test_hypt_part (
     id  NUMBER,
     t   DATE
 )
@@ -18,19 +18,29 @@ PARTITION p2021 VALUES LESS THAN (TO_DATE('01-Jan-2022','dd-MON-yyyy')) EXTERNAL
 PARTITION pmax VALUES LESS THAN (MAXVALUE) 
 ) INMEMORY;
 
-INSERT INTO test_hypt_partition VALUES (
+INSERT INTO test_hypt_part VALUES (
     10000,
     TO_DATE('01-Jan-2028', 'dd-MON-yyyy')
 );
 
-UPDATE test_hypt_partition
+UPDATE test_hypt_part
 SET
     t = TO_DATE('01-Jan-2029', 'dd-MON-yyyy')
 WHERE
     id = 10000;
 
 SET lines 200
+SET pages 9999
 COL partition_name FOR a14
-COL tablespace_name FOR a15
+COL high_value FOR a23
 COL table_name FOR a20
-SELECT table_name, partition_name, high_value, tablespace_name, read_only FROM user_tab_partitions;
+COL type_name FOR a20
+COL default_directory_name FOR a30
+SET ECHO ON
+
+SELECT table_name, partition_name, partition_position, high_value FROM user_tab_partitions;
+
+SELECT external, hybrid FROM user_tables WHERE table_name = 'TEST_HYPT_PART';
+
+SELECT table_name, type_name, default_directory_name FROM all_xternal_part_tables;
+
--- chap10/ext_partition_table.sql
+++ chap10/ext_partition_table.sql
@@ -2,9 +2,9 @@ DROP DIRECTORY default_dir;
 
 CREATE DIRECTORY default_dir AS '/tmp';
 
-DROP TABLE test_ext_partition;
+DROP TABLE test_ext_part;
 
-CREATE TABLE test_ext_partition (
+CREATE TABLE test_ext_part (
     id  NUMBER,
     t   DATE
 )
@@ -18,7 +18,7 @@ PARTITION p2020 VALUES LESS THAN (TO_DATE('01-Jan-2021','dd-MON-yyyy')) LOCATION
 PARTITION p2021 VALUES LESS THAN (TO_DATE('01-Jan-2022','dd-MON-yyyy')) LOCATION ('2021.csv')
 ) INMEMORY;
 
-INSERT INTO test_ext_partition VALUES (
+INSERT INTO test_ext_part VALUES (
     10000,
     TO_DATE('01-Feb-2021', 'dd-MON-yyyy')
 );
@@ -30,10 +30,11 @@ COL high_value FOR a23
 COL table_name FOR a20
 COL type_name FOR a20
 COL default_directory_name FOR a30
+SET ECHO ON
 
 SELECT table_name, partition_name, partition_position, high_value FROM user_tab_partitions;
 
-SELECT external, hybrid FROM user_tables WHERE table_name = 'TEST_EXT_PARTITION';
+SELECT external, hybrid FROM user_tables WHERE table_name = 'TEST_EXT_PART';
 
 SELECT table_name, type_name, default_directory_name FROM all_xternal_part_tables;
 
