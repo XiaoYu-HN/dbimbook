9d592cf200d6baa07dfa37eb26836a4e9f7689f2
XiaoYu-HN <xiaojinghang@foxmail.com>
2021-07-26 17:32:32 +0800

    add DBMS_CLOUD creat external table DDL

--- chap10/ext_bigdata_cloud.sql
+++ chap10/ext_bigdata_cloud.sql
@@ -20,6 +20,28 @@ ORGANIZATION EXTERNAL
     com.oracle.bigdata.csv.rowformat.quotecharacter='"'
    )
    location ('https://objectstorage.us-ashburn-1.oraclecloud.com/n/ocichina001/b/b01/o/customer.csv')
+--   location ('https://raw.githubusercontent.com/XiaoYu-HN/dbimbook/main/chap10/customer.csv')
   )  REJECT LIMIT UNLIMITED;
+
+select count(*) from customer_ext_bd_cloud;
+
+-- below is equivalent DDL
+drop table customer_ext_bd_cloud;
+
+BEGIN
+dbms_cloud.create_external_table(
+table_name => 'customer_ext_bd_cloud', 
+file_uri_list => 'https://objectstorage.us-ashburn-1.oraclecloud.com/n/ocichina001/b/b01/o/customer.csv',
+format => JSON_OBJECT('type' VALUE 'csv', 'skipheaders' VALUE '0'),
+column_list => 'c_custkey     NUMBER,
+    c_name        VARCHAR(25),
+    c_address     VARCHAR(25),
+    c_city        CHAR(10),
+    c_nation      CHAR(15),
+    c_region      CHAR(12),
+    c_phone       CHAR(15),
+    c_mktsegment  CHAR(10)');
+END;
+/
 
 select count(*) from customer_ext_bd_cloud;
