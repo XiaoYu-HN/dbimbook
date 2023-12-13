dfa8dd412c2d3fa2c1ae94f546dbb3ff22997897
Xiao Yu <xiaojinghang@foxmail.com>
2023-12-13 18:13:54 +0800

    Update dbim_ima_usage.sh
    support RAC

--- chap04/dbim_ima_usage.sh
+++ chap04/dbim_ima_usage.sh
@@ -5,6 +5,6 @@ sqlplus -S /nolog <<'EOF'
 COL pool FOR a10
 COL populate_status FORMAT a16
 
-SELECT pool, round(alloc_bytes/1024) as "ALLOC(KB)", round(used_bytes/1024) as "USED(KB)", populate_status, con_id FROM v$inmemory_area;
+SELECT inst_id, pool, round(alloc_bytes/1024) as "ALLOC(KB)", round(used_bytes/1024) as "USED(KB)", populate_status, con_id FROM gv$inmemory_area;
 
 EOF
