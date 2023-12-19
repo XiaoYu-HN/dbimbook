c00c524c837ff8faeb593f8fb24b4963bf0b2694
Xiao Yu <xiaojinghang@foxmail.com>
2023-12-19 20:28:11 +0800

    Update dbim_get_table_attr.sh
    change column display width to accomodate DUPLICATE ALL

--- chap04/dbim_get_table_attr.sh
+++ chap04/dbim_get_table_attr.sh
@@ -6,8 +6,8 @@ col segment_name for a16
 col partition_name for a16
 column inmemory_compression heading 'INMEMORY|COMPRESSION' format a18;
 column inmemory_priority heading 'INMEMORY|PRIORITY' format a10;
-column inmemory_duplicate heading 'INMEMORY|DUPLICATE' format a12;
-column inmemory_distribute heading 'INMEMORY|DISTRIBUTE' format a12;
+column inmemory_duplicate heading 'INMEMORY|DUPLICATE' format a16;
+column inmemory_distribute heading 'INMEMORY|DISTRIBUTE' format a16;
 set lines 140
 
 SELECT
