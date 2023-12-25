31b84362c56a4d7e8b3fe01566ae754556de05eb
Xiao Yu <xiaojinghang@foxmail.com>
2023-12-25 10:34:28 +0800

    Update dbim_get_popstatus_RAC.sh

--- chap04/dbim_get_popstatus_RAC.sh
+++ chap04/dbim_get_popstatus_RAC.sh
@@ -7,7 +7,7 @@ COL segment_name heading 'SEGMENT|NAME' FORMAT A16
 COL populate_status heading 'POPULATED|STATUS'
 COL "IN_MEM_SIZE(KB)" heading 'IN MEM|SIZE(KB)' FORMAT 999,999,999
 COL "ON_DISK_SIZE(KB)" heading 'ON DISK|SIZE(KB)' FORMAT 999,999,999
-COL bytes_not_populated heading 'BYTES NOT|POPULATED' 
+COL kb_not_populated heading 'KB NOT|POPULATED' 
 COL compression_ratio heading 'COMPRESSION|RATIO'
 SET LINESIZE 140
 SET PAGES 9999
@@ -19,7 +19,7 @@ SELECT
     inmemory_size / 1024                                    AS "IN_MEM_SIZE(KB)",
     bytes / 1024                                            AS "ON_DISK_SIZE(KB)",
     round((bytes - bytes_not_populated) / inmemory_size, 2) AS "COMPRESSION_RATIO",
-    bytes_not_populated,
+    bytes_not_populated/1224 as kb_not_populated,
     populate_status
 FROM
     gv$im_segments
